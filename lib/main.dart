bdceb63d1d6df25591632f6e05b3b11557fff2e0
Milad Rami <miladrami.dev@gmail.com>
2023-06-13 00:15:24 +0330

    update number two

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,145 @@
 import 'package:flutter/material.dart';
-import 'SecondRoute.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Named Routes Demo',
-      // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const FirstRoute(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => const SecondRoute(),
-      },
-    ),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'First Screen',
+    home: FirstScreen(),
+  ));
 }
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Landing Page App',
-      home: Scaffold(
-        body: ListView(
-          children: [
-            Container(
-                height: 754,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/background_imagee.jpg'),
-                    fit: BoxFit.fill,
+    final screenHeight = MediaQuery.of(context).size.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: (screenHeight - statusBarHeight),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background_imagee.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Stack(children: [
+              const Positioned(
+                left: 16,
+                right: 16,
+                top: 40,
+                child: Text(
+                  'Diva chat bot is a new way, a simple way, to access what you need, when you need it.',
+                  style: TextStyle(
+                    fontFamily: 'Source_Sans_Pro',
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                child: Stack(
-                  children: [
-                    const Positioned(
-                      left: 16,
-                      right: 16,
-                      top: 80,
+              ),
+              Positioned(
+                  left: 260,
+                  right: 40,
+                  bottom: 50,
+                  child: TextButton(
                       child: Text(
-                        'Diva chat bot is a new way,              a simple way,                                         to access what you need,          when you need it.',
+                        'Next',
                         style: TextStyle(
-                          fontFamily: 'Source_Sans_Pro',
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                            fontFamily: 'Source_Sans_Pro',
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SecondScreen()),
+                        );
+                      }))
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: (screenHeight - statusBarHeight),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background_image.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Stack(
+              children: [
+                const Positioned(
+                  left: 16,
+                  right: 16,
+                  top: 40,
+                  child: Text(
+                    'What you need is within your reach with just one click.',
+                    style: TextStyle(
+                      fontFamily: 'Source_Sans_Pro',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  left: 16,
+                  right: 16,
+                  bottom: 110,
+                  child: Text(
+                    'If you\'re looking for a new house or a new job, even if you\'re selling your house or need an expert to hire,    You Are One Click Away.',
+                    style: TextStyle(
+                      fontFamily: 'Source_Sans_Pro',
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 30,
+                  right: 30,
+                  bottom: 20,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      const url = 'https://hansa-tile.de/test10/';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: Text('Let\'s Talk'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade900,
+                      padding: const EdgeInsets.only(
+                          left: 120, right: 120, top: 20, bottom: 20),
+                      textStyle: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
                       ),
                     ),
-                    Positioned(
-                      left: 260,
-                      right: 40,
-                      bottom: 50,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/second');
-                        },
-                        child: const Text(
-                          'Next',
-                          style: TextStyle(
-                              fontFamily: 'Source_Sans_Pro',
-                              fontSize: 27,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-          ],
-        ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
